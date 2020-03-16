5a4bc686dda8bc4b047225b2aa735523f8ffba83
Mookiang <mookiang@gmail.com>
2020-03-16 14:14:01 +0900

    #add - koa-bodyparser 모듈 추가

--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
+const bodyParser = require('koa-bodyparser');
 const api = require('./api');
 const PORT = 3000;
 
 router.use('', api.routes());
+app.use(bodyParser());
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(PORT, () => {
     console.log(`Koa server is listening to port ${PORT}`);
 });
-
