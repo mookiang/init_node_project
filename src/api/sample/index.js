f3c1df786b3626f9dcb4794a76491c7ccfe19ecc
Mookiang <mookiang@gmail.com>
2020-03-16 14:17:23 +0900

    # add - Chatbot 테스트용 함수 추가

--- src/api/sample/index.js
+++ src/api/sample/index.js
@@ -3,5 +3,7 @@ const router = new Router();
 const service = require('./service');
 
 router.get('/', service.getSample);
+router.get('/chatbot', service.getChatbot);
+router.post('/chatbot', service.postChatbot);
 
 module.exports = router;
--- src/api/sample/service.js
+++ src/api/sample/service.js
@@ -4,4 +4,19 @@ module.exports = {
             ctx.body = "Hello World";
         } catch (e) { console.log(e); }
     },
+
+    async getChatbot(ctx) {
+        try {
+            ctx.body = 'Hello Chatbot!';
+        } catch (e) { console.log(e); }
+    },
+
+    async postChatbot(ctx) {
+        try {
+            console.log(ctx.request.body);
+            ctx.body = {
+                data: 'Chatbot - Data'
+            }
+        } catch (e) { console.log(e); }
+    }
 }
